14577452d7d6d02e22d0983cd120046b912a088d
Spencer-C-Barnes <scb9ca@virginia.edu>
2019-02-09 12:43:42 -0500

    Title Screen

---
+++ ReDrawn/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public void ChangeToScene(int sceneToChangeTo)
+    {
+        Application.LoadLevel(sceneToChangeTo);
+    }
+}
