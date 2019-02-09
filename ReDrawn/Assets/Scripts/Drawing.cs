7b965a11721026deec7a4b964290a85579875ed5
Omar El Sheikh <techmanozmar@gmail.com>
2019-02-09 12:53:57 -0500

    Initial Drawing Implementation!!!
    Created Line Segment Prefab as well

---
+++ ReDrawn/Assets/Scripts/Drawing.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawing : MonoBehaviour
+{
+    public float nextPlaceTime = 0;
+    public float secondsBetweenPlacement = 1;
+    public GameObject lineSegmentPrefab;
+    private Vector3 lastPos;
+    public Vector3 mouseVelocity = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseVelocity = Input.mousePosition - lastPos;
+        lastPos = Input.mousePosition;
+        print(mouseVelocity);
+
+        if (Input.GetButton("Fire1") && mouseVelocity.sqrMagnitude > 0)
+        {
+            if (nextPlaceTime < Time.time)
+            {
+                GameObject temp = Instantiate(lineSegmentPrefab);
+                Vector3 tempPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                temp.transform.position = new Vector3(tempPos.x, tempPos.y);
+                float angle = Mathf.Atan2(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")) * Mathf.Rad2Deg;
+                temp.transform.Rotate(0, 0, angle);
+                nextPlaceTime = Time.time + secondsBetweenPlacement;
+            }
+        }
+    }
+}
