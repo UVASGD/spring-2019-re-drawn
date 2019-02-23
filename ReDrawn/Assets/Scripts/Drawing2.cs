bc6bbd1e6329de1b1b8ddcdbd857fc51a6f4243b
Omar El Sheikh <techmanozmar@gmail.com>
2019-02-23 12:41:29 -0500

    DRAWING WOOP WOOP
    Implemented basic drawing tool
    Added Physics Simulation to the drawn object

--- ReDrawn/Assets/Scripts/Drawing2.cs
+++ ReDrawn/Assets/Scripts/Drawing2.cs
@@ -8,12 +8,12 @@ public class Drawing2 : MonoBehaviour
     public float secondsBetweenPlacement = 1;
     public GameObject DrawingPrefab;
     public GameObject currentDrawing;
-    private Vector3 lastPos;
-    public Vector3 mouseVelocity = new Vector3(0, 0, 0);
     public float lineThickness = .25f;
+    private Vector3 lastPos;
+    private Vector3 mouseVelocity = new Vector3(0, 0, 0);
 
-    public List<Vector2> polygonPointsStart = new List<Vector2>();
-    public List<Vector2> polygonPointsEnd = new List<Vector2>();
+    private List<Vector2> polygonPointsStart = new List<Vector2>();
+    private List<Vector2> polygonPointsEnd = new List<Vector2>();
 
     // Start is called before the first frame update
     void Start()
@@ -26,13 +26,11 @@ public class Drawing2 : MonoBehaviour
     {
         mouseVelocity = Input.mousePosition - lastPos;
         lastPos = Input.mousePosition;
-        print(mouseVelocity);
 
         if (Input.GetButtonDown("Fire1"))
         {
             currentDrawing = Instantiate(DrawingPrefab);
-            Vector3 tempPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            currentDrawing.transform.position = new Vector3(tempPos.x, tempPos.y);
+            currentDrawing.transform.position = Vector3.zero;
         }
 
         else if (Input.GetButton("Fire1") && mouseVelocity.sqrMagnitude > 0)
@@ -54,16 +52,18 @@ public class Drawing2 : MonoBehaviour
 
         else if (Input.GetButtonUp("Fire1"))
         {
-            print("asdfadfasf");
             PolygonCollider2D temp = currentDrawing.GetComponent<PolygonCollider2D>();
             Vector2[] colliderPoints = new Vector2[polygonPointsStart.Count * 2];
             for (int x = 0; x < colliderPoints.Length / 2; x++)
             {
                 colliderPoints[x] = polygonPointsStart[x];
-                colliderPoints[colliderPoints.Length - x] = polygonPointsEnd[x];
+                colliderPoints[colliderPoints.Length - x - 1] = polygonPointsEnd[x];
             }
-
             temp.points = colliderPoints;
+            Rigidbody2D rb = currentDrawing.GetComponent<Rigidbody2D>();
+            rb.simulated = true;
+            polygonPointsStart.Clear();
+            polygonPointsEnd.Clear();
         }
     }
 }
