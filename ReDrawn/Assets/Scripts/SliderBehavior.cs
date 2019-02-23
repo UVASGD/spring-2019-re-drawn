67286cebfbacf6406fb64643de6a97903c0d2abc
Spencer-C-Barnes <scb9ca@virginia.edu>
2019-02-23 12:15:50 -0500

    Drawing Slider

--- ReDrawn/Assets/Scripts/SliderBehavior.cs
+++ ReDrawn/Assets/Scripts/SliderBehavior.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SliderBehavior : MonoBehaviour
 {
+    public float currentDrawing = 100f;
+    public float maxDrawing = 100f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        drawingBar.value = drawingBarPercent();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            currentDrawing -= Time.deltaTime*60;
+        }
+        drawingBar.value = drawingBarPercent();
+    }
+
+    public Slider drawingBar;
+    float drawingBarPercent()
+    {
+        return currentDrawing / maxDrawing;
     }
 }
