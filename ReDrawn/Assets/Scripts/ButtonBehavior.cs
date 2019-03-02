6078a413e9d4fe5003c867b046c0ac743153fd5b
Spencer-C-Barnes <scb9ca@virginia.edu>
2019-03-02 11:51:53 -0500

    Pause Menu... ish

--- ReDrawn/Assets/Scripts/ButtonBehavior.cs
+++ ReDrawn/Assets/Scripts/ButtonBehavior.cs
@@ -3,8 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class ButtonBehavior : MonoBehaviour
 {
-    public void ChangeToScene(int sceneToChangeTo)
+    public void ChangeToScene(string sceneToChangeTo)
     {
-        Application.LoadLevel(sceneToChangeTo);
+        SceneManager.LoadScene(sceneToChangeTo);
     }
 }
