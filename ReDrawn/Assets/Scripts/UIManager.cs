693155ebdfb8437e0e5f8491ef1544d35cc90e36
Spencer-C-Barnes <scb9ca@virginia.edu>
2019-03-02 12:20:40 -0500

    Pause Menu (pretty much done)

---
+++ ReDrawn/Assets/Scripts/UIManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    GameObject[] pauseObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+    }
+
+    // Update is called once per frame
+    public void CheckPaused(bool paused)
+    {
+        //uses the p button to pause and unpause the game
+        if (paused)
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Debug.Log("high");
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
+    //Reloads the Level
+    public void Reload()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    //controls the pausing of the scene
+    public void pauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    //shows objects with ShowOnPause tag
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    //loads inputted level
+    public void LoadLevel(string level)
+    {
+        Application.LoadLevel(level);
+    }
+}
