a2cc6db26d68248340b5a76343faec2e667be222
Jared Capelle <jjc2bd@virginia.edu>
2019-03-29 21:22:41 -0400

    Weird GitHub Stuff
    Also Made Drop-Through Platforms

---
+++ ReDrawn/Assets/Scripts/DropThroughPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropThroughPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && Input.GetAxis("Vertical") < 0)
+        {
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            Invoke("ReEnableCollider", 0.5f);
+        }
+    }
+
+    void ReEnableCollider()
+    {
+        gameObject.GetComponent<Collider2D>().enabled = true;
+    }
+}
