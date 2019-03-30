64985e47fb359e70e522caa8812bae57bf2114f4
Jared Capelle <jjc2bd@virginia.edu>
2019-03-29 20:20:08 -0400

    Made out of bounds zone

---
+++ ReDrawn/Assets/Scripts/OffZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 initialPosition;
+    void Start()
+    {
+        initialPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.transform.position = initialPosition;
+        }
+    }
+}
