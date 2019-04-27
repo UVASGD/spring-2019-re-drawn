97805309dbc9bc7c127c807b8bbf4aa4686d7d30
Clara Kim <cjk8ad@virginia.edu>
2019-04-27 12:57:50 -0400

    collecty bois

---
+++ ReDrawn/Assets/Collectable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectable : MonoBehaviour
+{
+    private static int count;
+    public Text countText;
+    public static int numTotal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        countText = GameObject.Find("CountText").GetComponent<Text>();
+        setCountText();
+        numTotal = GameObject.FindGameObjectsWithTag("Collectable").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print("Count: " + count);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if (this.tag == "Collectable")
+            {
+                Destroy(gameObject);
+                count++;
+                setCountText();
+                print("Count: " + count); //just testing some stuff
+            }
+        }
+    }
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+}
